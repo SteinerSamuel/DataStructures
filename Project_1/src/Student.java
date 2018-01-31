064c632805407e71486ba40fee00872d81ee6769
SteinerSamuel <31547643+SteinerSamuel@users.noreply.github.com>
2018-01-31 12:57:37 -0500

    Add files via upload

--- Project_1/src/Student.java
+++ Project_1/src/Student.java
@@ -17,12 +17,7 @@ public class Student{
 	// constructor for student which takes the student id, name and the grades for the quizzes and exams
 	public Student (String id, String n, int q1, int q2, int m, int f)
 	{
-		studentID = id;
-		name = n;
-		quiz1 = q1;
-		quiz2 = q2;
-		midterm = m;
-		finalexam = f;
+		this(  id,  n,  q1,  q2,  m,  f, 0.0, "na")
 	}
 	
 	// constructor for student which takes student id, name, the grades for the quizzes, the exams, the average grade, and the letter grade
